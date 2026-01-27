4038ca7ac936384ab0ecf7bf1b8cc461e602f361
Aditya Jain <jainaditya412@gmail.com>
2026-01-27 09:55:48 +0530

    Create 1.1.cpp
    First lec content

---
+++ 1.1.cpp
@@ -0,0 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    
+    cout << "1" << endl;//print 1 on the screen
+    
+    cout << "2" << endl;
+    cout << "3\n";
+    cout << "3" << "\n";
+    cout << "4" << endl;
+    cout << "(1+2)" << endl;
+    
+    /* Performing mathematical operations using C++
+        such as: +, -, /, *
+    */
+    
+    cout << (1+2) << endl;
+    cout << 6 << endl;
+    cout << 2+3 << endl;
+    cout << 2*3 << endl;
+    cout << 2-3 << endl;
+    cout << 2/3 << endl;
+    cout << 3/2 << endl;
+    cout << 4/3 << endl;
+    cout << 4/2 << endl;
+    cout << 3.0/2 << endl;
+    
+    /*
+    data types in c++
+    int --> 1, 2, -4, 0
+    double --> 3.5, -1.6
+    bool --> true, false
+    char --> 'a', 'b' , 'z', 'D'
+    string --> "a", "adit123", "aditya", "Aditya Jain" 
+    */
+    
+    /*
+    Variables in C++
+    
+    int a = 5;
+   
+    int a;
+    a = 5;
+    */
+    
+    // cout << a << endl;
+    int a = 10;
+    a = 95;
+    // a = "aditya";
+    cout << a << endl;
+    
+    return 0;
+}
